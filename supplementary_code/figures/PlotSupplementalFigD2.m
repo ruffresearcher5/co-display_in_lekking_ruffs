dad472addca33718146dbb9d9ee2377664345a96
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2021-11-02 18:50:30 +0200

    Update PlotSupplementalFigD2.m

--- supplementary_code/figures/PlotSupplementalFigD2.m
+++ supplementary_code/figures/PlotSupplementalFigD2.m
@@ -1,4 +1,4 @@
-%%%%%Script for ploting supplementary figure C2%%%%%%
+%%%%%Script for ploting supplementary figure D2%%%%%%
 
 Figure_D2=figure;
     Plot_A_a=plotGr_sens_sat("strong");
@@ -25,8 +25,21 @@ Figure_D2=figure;
  text(-57.5,0.55,{'Weak'},'Color','black','FontWeight',...
      'bold','FontSize',18)
  
- %%%Note that to produce the graph from the manuscript the plot edit 
-    %should be used to change the colormaps of the Resident copulation 
-    %gainsubplots. The markers should be changed in the following ways: 
-    %1) add markers to indicies 7 and 24; 2)the marker at index 17 should 
-    %be deleted; 3) the marker at index 24 should be moved to index 8. 
+ %%%Note that to produce the graph from the presented in the supplementary
+    %the plot edit should be used to change the colormaps of the subplots.
+    %The subplots should be changed by eliminating the
+    %markers at all the indicies with the colormap editor then placing
+    %markers at the follwing locations with the given colors: 
+    
+    %%%%resident reward subplot color maps: Index 1  #808080, 
+    %Index 19  #d4d4d4, Index 22  white, Index 26  #d9faff, 
+    %Index 33  #adf5ff, Index 44  #73bdf5, Index 49  #2487bd, 
+    %Index  54  #0070a3, Index 59   #00406e, Index 64   #000326
+    
+    %%%%satellite reward subplots: Index 1  Index 1 --> white,
+    %Index 4  #ffebed, Index 16  #ffccd1, Index 32  #bf8282,
+    %Index 40  #d63d4f, Index 48  #960024, Index 56  #78000f, 
+    %Index 64  #1c0000
+ 
+
+
