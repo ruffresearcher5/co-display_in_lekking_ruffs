fe6a9524d78b7324a18ecfa099122d7ddcccc53c
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2021-11-03 15:56:15 +0200

    Update and rename supplementary_code/figures/plotDisrupRisk.m to supplementary_code/figures/figure_B2/plotDisrupRisk.m

--- supplementary_code/figures/figure_B2/plotDisrupRisk.m
+++ supplementary_code/figures/figure_B2/plotDisrupRisk.m
@@ -1,5 +1,5 @@
 function DrPlot = plotDisrupRisk()
-%%%%%Subplot function for supplementary figure B2_C%%%%%
+%%%%%Subplot function for supplementary figure B2_B%%%%%
 %Plot of assumed function for resident disruption risk. 
 Comp_level="medium";
 %Preduction matrix
