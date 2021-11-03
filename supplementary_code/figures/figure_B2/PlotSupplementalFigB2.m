7aee448584f6ecad98f8c6c7a47691b48c2d0482
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2021-11-03 16:00:06 +0200

    Update PlotSupplementalFigB2.m

--- supplementary_code/figures/figure_B2/PlotSupplementalFigB2.m
+++ supplementary_code/figures/figure_B2/PlotSupplementalFigB2.m
@@ -3,3 +3,11 @@
 Figure_B2=figure;
     Plot_A=plotMonoCoeff();
     Plot_B=plotDisrupRisk();
+     %%%Note that to produce the graph from the presented in the supplementary
+    %the plot edit should be used to change the colormaps of the subplots.
+    %The subplots should be changed by eliminating the
+    %markers at all the indicies with the colormap editor then placing
+    %markers at the follwing locations with the given colors: 
+    %Index 1 --> white, Index 6 -->  #d9faff, Index 16 -->  #adf5ff,
+    %Index 32 -->  #73bdf5, Index 40 -->  #2487bd, Index 48 -->  #0070a3
+    %Index 56 -->  #00406e, Index 64 -->  #000326
