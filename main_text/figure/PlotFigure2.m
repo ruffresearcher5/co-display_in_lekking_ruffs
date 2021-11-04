245c28379a7b63e3dbaa1ee0f6c9bfd9343eb85d
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2021-11-04 14:12:36 +0200

    Update PlotFigure2.m

--- main_text/figure/PlotFigure2.m
+++ main_text/figure/PlotFigure2.m
@@ -4,21 +4,26 @@ Figure_2=figure;
     Plot_B=plotSatGs_main_text();
     Plot_C=plotSatProportion_main_text();
     Plot_D=PlotSkewMeanSat( );
-%annotations for max Gr and Gs
-    %max Gr
-    text(1.0,-1,{'Max\rm(\itr \rm= \alpha, \itL \rm = 3)'},...
-           'Color','black','FontWeight','bold','FontSize',12)
-    %annotation('arrow',[0.3231 0.3231],[.945 .905])
-    %max Gs
-    %annotation('rectangle',[0.6269 0.7345 0.02 0.04],'Color','red',...
-       % 'FaceColor','red','FaceAlpha',.3)
-    %text(1.1,-6.72,0,{'Max';'\rm(\itr \rm= \delta, \itL \rm = 4)'},...
-            %'Color','black','FontWeight','bold','FontSize',12)
-    %annotation('arrow',[0.605 0.6268],[0.754 0.754])
+
+ %%%Note that to produce the graph from the presented in the supplementary
+    %the plot edit should be used to change the colormaps of the subplots.
+    %The subplots should be changed by eliminating the
+    %markers at all the indicies with the colormap editor then placing
+    %markers at the follwing locations with the given colors: 
     
-  %%%Note that to produce the graph from the manuscript the plot edit 
-    %should be used to change the colormaps of the Resident copulation 
-    %gain subplots. The markers should be changed in the following ways: 
-    %1) put a marker at index 25,; 2) remove marker at index 17;
-    %move index 25 it to index 2; 3)change the first index to 
-    %R = 1, G = 0.7 and B = 0;
+    %%%%resident reward subplot colormaps: Index 1 --> #808080, 
+    %Index 19 --> #d4d4d4, Index 22 --> white, Index 26 --> #d9faff, 
+    %Index 33 --> #adf5ff, Index 44 --> #73bdf5, Index 49 --> #2487bd, 
+    %Index  54 --> #0070a3, Index 59 --> #00406e, Index 64 --> #000326
+    
+    %%%%satellite reward subplots colormaps: Index 1 --> white,
+    %Index 4 --> #ffebed, Index 16 --> #ffccd1, Index 32 --> #bf8282,
+    %Index 40 --> #d63d4f, Index 48 --> #960024, Index 56 --> #78000f, 
+    %Index 64 --> #1c0000
+    
+    %%%%satellite proportion subplot colormaps: Index 1 --> #000326,
+    %Index 4 --> #00406e, Index 8 --> #0070a3, Index 12 --> #2487bd,
+    %Index 16 --> #73bdf5, Index 24 --> #adf5ff, Index 30 --> #d9faff,
+    %Index 32 --> white, Index 35 --> #ffebed, Index 40 --> #ffccd1,
+    %Index 48 --> #bf8282, %Index 52 --> #d63d4f, Index 56 --> #960024,
+    %Index 60 --> #78000f, Index 64 --> #1c0000
