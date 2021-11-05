b671a0bd3a91032b933f238949cd7e72c8ac39b5
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2021-11-05 17:51:38 +0200

    Update and rename PlotSupplementalFigB7.m to PlotSupplementalFigB6.m

--- supplementary_code/figures/figure_B6/PlotSupplementalFigB6.m
+++ supplementary_code/figures/figure_B6/PlotSupplementalFigB6.m
@@ -1,6 +1,6 @@
 %%%%%Script for ploting supplementary figure B6%%%%%%
 
-Figure_B7=figure;
+Figure_B6=figure;
     Plot_A=PlotSkew_diff_null( );
     Plot_B=PlotSkew_diff_up( );
     Plot_C=PlotSkew_diff_skew( );
