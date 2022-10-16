877d5d345182f6791d8b1eac38123323fae9828f
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2022-10-16 23:03:31 +0300

    Update plotDeltaSkew_sens_sat.m

--- appendix_code/figures/figure_A10/plotDeltaSkew_sens_sat.m
+++ appendix_code/figures/figure_A10/plotDeltaSkew_sens_sat.m
@@ -1,5 +1,5 @@
 function SkewPlot = plotDeltaSkew_sens_sat(Sat_strength)
-%%%%%Subplot function for supplementary figure D2_D_strong-weak%%%%%
+%%%%%Subplot function for supplementary figure A10_d_strong-weak%%%%%
 %Plot of predicted skew when a co-display is present on the lek. Plot is
 %specified for different satellite streangths: strong, medium, and weak.
   
@@ -27,7 +27,7 @@ y_limits=[0 1.0];
 x_ticks=[1 2 3 4 5 6 7];
 y_ticks=[0.00 0.50 1.00];
 X_tick_labels=["1" "" "3" "" "5" "" "7"];
-titles_spot=[-1.9 1.04];
+titles_spot=[1.1 1];
 x_axis_tit="Residents/lek";
 y_axis_tit="Resident skew";
 %Make a figure
@@ -49,8 +49,9 @@ switch Sat_strength
         yticks(y_ticks)
         xticklabels(X_tick_labels)
         set(SkewPlot,'Fontsize',13)
-        title("D",'FontSize',24)
-        title ( 'D' , 'position' , titles_spot)
+        title("(d)",'FontSize',24, 'FontWeight',...
+            'Normal')
+        title ( '(d)' , 'position' , titles_spot)
         ylabel(y_axis_tit,'FontSize',13)
         set(SkewPlot,'box','off','TickDir','out','yticklabel',num2str(get(gca,'ytick')','%.1f'))
         set(SkewPlot,'Position',[0.78 0.7093 0.0961 0.21])
