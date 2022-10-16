dd583d6eb428fafac93b6b012bfce312d139b87f
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2022-10-16 22:33:26 +0300

    Update PlotSupplementalFigA5.m

--- appendix_code/figures/figure_A5/PlotSupplementalFigA5.m
+++ appendix_code/figures/figure_A5/PlotSupplementalFigA5.m
@@ -1,6 +1,6 @@
-%%%%%Script for ploting supplementary figure B5%%%%%%
+%%%%%Script for ploting supplementary figure A5%%%%%%
 
-Figure_B5=figure;
+Figure_A5=figure;
     Plot_A=plotSatGs_Null();
     Plot_B=plotSatGs_UP();
     Plot_C=plotSatGs_Sk();
@@ -15,16 +15,4 @@ Figure_B5=figure;
     set(hr1,'TickDir','out','xticklabel',num2str(get(hr1,'xtick')',...
             '%.2f'))
 
-   %%%Note that to produce the graph from the presented in the supplementary
-    %the plot edit should be used to change the colormaps of the subplots.
-    %The subplots should be changed by eliminating the
-    %markers at all the indicies with the colormap editor then placing
-    %markers at the follwing locations with the given colors: 
     
-    
-    %%%%satellite reward subplots colormaps: Index 1 --> white,
-    %Index 4 --> #ffebed, Index 16 --> #ffccd1, Index 32 --> #bf8282,
-    %Index 40 --> #d63d4f, Index 48 --> #960024, Index 56 --> #78000f, 
-    %Index 64 --> #1c0000  
-
-   
