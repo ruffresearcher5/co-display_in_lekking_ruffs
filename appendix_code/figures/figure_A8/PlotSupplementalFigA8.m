692b78c9a5082b8738c6f0f2a80f45ebea2b33d9
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2022-10-16 22:48:41 +0300

    Update PlotSupplementalFigA8.m

--- appendix_code/figures/figure_A8/PlotSupplementalFigA8.m
+++ appendix_code/figures/figure_A8/PlotSupplementalFigA8.m
@@ -1,13 +1,6 @@
 %%%%%Plot monopolization coefficient and disruptive risk as related to lek
 %%%%%size and resident hierarchical rank%%%%%
-Figure_B8=figure;
+
+Figure_A8=figure;
     Plot_A=plotMonoCoeff();
     Plot_B=plotDisrupRisk();
-     %%%Note that to produce the graph from the presented in the supplementary
-    %the plot edit should be used to change the colormaps of the subplots.
-    %The subplots should be changed by eliminating the
-    %markers at all the indicies with the colormap editor then placing
-    %markers at the follwing locations with the given colors: 
-    %Index 1 --> white, Index 6 -->  #d9faff, Index 16 -->  #adf5ff,
-    %Index 32 -->  #73bdf5, Index 40 -->  #2487bd, Index 48 -->  #0070a3
-    %Index 56 -->  #00406e, Index 64 -->  #000326
