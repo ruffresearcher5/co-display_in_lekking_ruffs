51307eb995d3a25d0bcf7e2a402cea332f1a198d
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2022-10-16 21:57:42 +0300

    Add files via upload

---
+++ appendix_code/figures/PlotAppendixFigA13.m
@@ -0,0 +1,9 @@
+%%%%%Script for ploting supplementary figure A13 from manuscript with %
+%   running head Running head: Co-display in lekking ruffs%%%%%%
+
+Figure_A13=figure;
+    Plot_A=plotMonovResgains();
+    Plot_B=plotMonovSatgains();
+    Plot_C=plotMonovSatProp();
+    Plot_D=plotMonovSkewChange();
+
