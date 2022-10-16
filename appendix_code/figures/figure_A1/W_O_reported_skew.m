b2f5a01cd952f2f9486f9f84265cc5899e6a5829
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2022-10-16 22:09:14 +0300

    Update W_O_reported_skew.m

--- appendix_code/figures/figure_A1/W_O_reported_skew.m
+++ appendix_code/figures/figure_A1/W_O_reported_skew.m
@@ -2,7 +2,7 @@ function B_WO = W_O_reported_skew( L )
 %Widemo and Owens's (1995) reported relationship between
 %resident reproductive (mating) skew and mean number of residents on a lek,
 %L.
-%   See supplementary material, appendix B, subsection Mating skew, 
+%   See supplementary material, appendix 2, subsection Mating skew, 
 %   total copulations, monopolization coefficient, 
 %   and disruption risk relationships for more details.
 
