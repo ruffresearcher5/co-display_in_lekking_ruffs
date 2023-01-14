aca4fecc9b0fa628d056d4102a9b80e1f9c47804
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2023-01-14 19:40:28 +0200

    Update plotRSkPercSatCop_sens.m

--- appendix_code/figures/figure_A14/plotRSkPercSatCop_sens.m
+++ appendix_code/figures/figure_A14/plotRSkPercSatCop_sens.m
@@ -1,5 +1,5 @@
 function SatPerPlot = plotRSkPercSatCop_sens( Sat_Com_ab )
-%%%%%Subplot function for supplementary figure D6_D_weak-strong%%%%%
+%%%%%Subplot function for supplementary figure A14_d_weak-strong%%%%%
 %Plot of percent of copulations on a lek the satellite can 
 %obtain when co-displaying. Plot is specified for different 
 %resident satellite strengths levels: weak, medium, and strong. This is 
@@ -55,7 +55,7 @@ MatPercCopSat(1:7,7)=[CopSatPerc(7,1,h,Sat_Com_ab,Comp_level),...
 X_axesLimits=[0 8];
 Y_axesLimits=[0 8];
 XandY_ticks=[1 3 5 7];
-Title_position=[-1.5 0 1.1];
+Title_position=[0.75 0 1.1];
 X_label="Residents/lek";
 
 %Make a figure
@@ -68,23 +68,27 @@ switch Sat_Com_ab
                 xticks(XandY_ticks)
                 yticks(XandY_ticks)
                 yticklabels(["" "" "" ""])
-                text(-0.7,0.87,0,'\alpha','Color','black','FontSize',13)
-                text(-0.7,2.87,0,'\gamma','Color','black','FontSize',13)
-                text(-0.7,4.87,0,'\epsilon','Color','black','FontSize',13)
-                text(-0.7,6.87,0,'\eta','Color','black','FontSize',13)
+        text(-1,0.75,0,'\alpha','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,1.8,0,'\beta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,2.75,0,'\gamma','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.91,3.9,0,'\delta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.91,4.75,0,'\epsilon','color',[0.0 0.0 0.0]','FontSize',21)
+        text(-0.91,5.78,0,'\zeta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,6.75,0,'\eta','color',[0.0 0.0 0.0],'FontSize',21)
                 az = 0;
                 el = 90;
                 view(az, el);
                 a = get(SatPerPlot,'XTickLabel'); 
-                set(SatPerPlot,'XTickLabel',a,'Fontsize',11)
-                Title_plot="D";
+                set(SatPerPlot,'XTickLabel',a,'Fontsize',15)
+                Title_plot="(d)";
                 title(Title_plot,'FontSize',24)
-                title (Title_plot, 'position' , Title_position)   
-                colormap(SatPerPlot,flipud(pink));
+                title (Title_plot, 'position' , Title_position,'FontWeight',...
+            'Normal')   
+                colormap(colormap_sat_percent( ));
                 set(SatPerPlot,'Position',[0.62 0.7093 0.1566 0.2157])
                 caxis manual
-                caxis([0 36]);
-                
+                caxis([0 20]);
+                grid off
      case "medium"
         SatPerPlot=subplot(3,4,8);
                 Cbar3(MatPercCopSat,MatPercCopSat)
@@ -93,19 +97,23 @@ switch Sat_Com_ab
                 xticks(XandY_ticks)
                 yticks(XandY_ticks)
                 yticklabels(["" "" "" ""])
-                text(-0.7,0.87,0,'\alpha','Color','black','FontSize',13)
-                text(-0.7,2.87,0,'\gamma','Color','black','FontSize',13)
-                text(-0.7,4.87,0,'\epsilon','Color','black','FontSize',13)
-                text(-0.7,6.87,0,'\eta','Color','black','FontSize',13)
+        text(-1,0.75,0,'\alpha','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,1.8,0,'\beta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,2.75,0,'\gamma','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.91,3.9,0,'\delta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.91,4.75,0,'\epsilon','color',[0.0 0.0 0.0]','FontSize',21)
+        text(-0.91,5.78,0,'\zeta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,6.75,0,'\eta','color',[0.0 0.0 0.0],'FontSize',21)
                 az = 0;
                 el = 90;
                 view(az, el);
                 a = get(SatPerPlot,'XTickLabel'); 
-                set(SatPerPlot,'XTickLabel',a,'Fontsize',11)
+                set(SatPerPlot,'XTickLabel',a,'Fontsize',15)
                 set(SatPerPlot,'Position',[0.62 0.4096 0.1566 0.2157])
-                colormap(SatPerPlot,flipud(pink));
+                colormap(colormap_sat_percent( ));
                 caxis manual
-                caxis([0 36]);
+                caxis([0 20]);
+                grid off
       case "weak"
         SatPerPlot=subplot(3,4,12);
                 Cbar3(MatPercCopSat,MatPercCopSat)
@@ -115,20 +123,23 @@ switch Sat_Com_ab
                 xticks(XandY_ticks)
                 yticks(XandY_ticks)
                 yticklabels(["" "" "" ""])
-                text(-0.7,0.87,0,'\alpha','Color','black','FontSize',13)
-                text(-0.7,2.87,0,'\gamma','Color','black','FontSize',13)
-                text(-0.7,4.87,0,'\epsilon','Color','black','FontSize',13)
-                text(-0.7,6.87,0,'\eta','Color','black','FontSize',13)
+        text(-1,0.75,0,'\alpha','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,1.8,0,'\beta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,2.75,0,'\gamma','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.91,3.9,0,'\delta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.91,4.75,0,'\epsilon','color',[0.0 0.0 0.0]','FontSize',21)
+        text(-0.91,5.78,0,'\zeta','color',[0.0 0.0 0.0],'FontSize',21)
+        text(-0.96,6.75,0,'\eta','color',[0.0 0.0 0.0],'FontSize',21)
                 az = 0;
                 el = 90;
                 view(az, el);
                 a = get(SatPerPlot,'XTickLabel'); 
-                set(SatPerPlot,'XTickLabel',a,'Fontsize',11)
+                set(SatPerPlot,'XTickLabel',a,'Fontsize',15)
                 set(SatPerPlot,'Position',[0.62 0.1100 0.1566 0.2157])
-                colormap(SatPerPlot,flipud(pink));
+                colormap(colormap_sat_percent( ));
                 caxis manual
-                caxis([0 36]);
-                
+                caxis([0 20]);
+                grid off
 
 
 end
