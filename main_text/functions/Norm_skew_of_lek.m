514a52c47f2d784171f54eeb880a290ecc617e2b
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2023-01-14 16:10:03 +0200

    Update Norm_skew_of_lek.m

--- main_text/functions/Norm_skew_of_lek.m
+++ main_text/functions/Norm_skew_of_lek.m
@@ -1,17 +1,10 @@
-function Sd = NewSkewLM( L,r,h, Sat_strength,Comp_level)
-%function provides the resident reproductive skew when a satellite
-%co-displays with a resident of r rank on a lek with L residents under one
-%of the four fitness loss scenarios h when the satellite has a competitive
-%ability Sat_strength and competition between residents is Comp_level.
-
-if r == 1
-
-    alphacops=Res_payoffs( L,r,h,Sat_strength,Comp_level);
-else
-    alphacops=Single_res_cops(L,1,h);
-end
-Sd=alphacops/(Total_cops_on_lek( L )-Satellite_payoffs( L,r,h,Sat_strength,Comp_level ));
-
+function B_Hat = Norm_skew_of_lek( L )
+%Returns the normalized resident reproductive skew for a lek size with L 
+%residents on it.
+%   See methods and table 1, element 6 in the main text for full
+%   description and formulation.
+B = Skew_of_lek( L );
+B_Hat = B/(1-(1-B)^L);
 
 end
 
