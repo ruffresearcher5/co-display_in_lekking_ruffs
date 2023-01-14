c8b33aa99a7654657b1e7390addc2d073e99b922
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2023-01-14 16:56:31 +0200

    Update and rename PlotSupplementalFigA14.m to PlotAppendixFigA14.m

--- appendix_code/figures/figure_A14/PlotAppendixFigA14.m
+++ appendix_code/figures/figure_A14/PlotAppendixFigA14.m
@@ -1,6 +1,6 @@
-%%%%%Script for ploting supplementary figure D6%%%%%%
+%%%%%Script for ploting supplementary figure A14%%%%%%
 
-Figure_D6=figure;
+Figure_A14=figure;
     Plot_A_a=plotNull_SatCopPerc_sens("strong");
     Plot_A_b=plotNull_SatCopPerc_sens("medium");
     Plot_A_c=plotNull_SatCopPerc_sens("weak");
@@ -24,16 +24,10 @@ Figure_D6=figure;
                         'Color','black','FontSize',18,'Rotation',90)
                     set(hr1,'TickDir','out','xticklabel',...
                         num2str(get(hr1,'xtick')','%.1f'))
-    
-   text(12.5,1.5,0,{'< 10%'},'Color',[0.5 0.5 0.5],'FontSize',20,'Rotation',90)
+   text(12.5,1.5,0,{'< 10%'},'Color',[0.5 0.5 0.5],'FontSize',20,...
+   'Rotation',90)
    text(12.5,-13,0,[char(8805) '10%'],...
             'Color',[0.8359 0.2431 0.3098],'FontSize',20,'Rotation',90)
-   %%%Note that to produce the graph from the manuscript the plot edit 
-    %should be used to change the colormaps of the subplots. The color map
-    %has two colors gray (##a6a6a6) and red (#D53E4F). For all subplots change
-    %color indicies. Make sure limits are 0 and 20 and then set the
-    %indicies 32 and 64 to the red color above and 1 and 31 to the gray color. 
-    %Remove all color markers but those that you set on 31 and 32.
     
     
  %add satellite strength levels
@@ -43,3 +37,4 @@ Figure_D6=figure;
      'bold','FontSize',18)
  text(-45.5,3.78,0,{'Weak'},'Color','black','FontWeight',...
      'bold','FontSize',18)
+    
