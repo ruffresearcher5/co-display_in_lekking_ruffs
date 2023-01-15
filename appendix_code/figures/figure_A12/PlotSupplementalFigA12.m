0a5550db7187ec1ca3ddab930691e4454aef9480
ruffresearcher5 <49790259+ruffresearcher5@users.noreply.github.com>
2023-01-15 23:40:43 +0200

    Update and rename PlotSupplementalFigA12.m to PlotAppendixFigA12.m

--- appendix_code/figures/figure_A12/PlotSupplementalFigA12.m
+++
@@ -1,9 +0,0 @@
-%%%%%Script for ploting supplementary figure D4 from manuscript with %
-%   running head Running head: Co-display in lekking ruffs%%%%%%
-
-Figure_D4=figure;
-    Plot_A=plotMonovResgains();
-    Plot_B=plotMonovSatgains();
-    Plot_C=plotMonovSatProp();
-    Plot_D=plotMonovSkewChange();
-
